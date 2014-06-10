790ca1ecc4b64efd4c765bd6ad8757103dcfdd54
drytuna <drytuna@gmail.com>
2014-06-09 20:11:28 -0500

    tested connection

--- echo_main.py
+++ echo_main.py
@@ -3,14 +3,16 @@ import echo_client
 import pdb
 import sys
 
-from multiprocessing import Process
+from multiprocessing import Process, Value
 
 a = echo_server.server_start()
 
 while True:
     message = str(sys.stdin.readline())
-    server_process = Process(target = echo_server.server_run ,args = (a,))
-    client_process = Process(target = echo_client.echo_client ,args = (message,))
+    server_process = Process(target = echo_server.server_run,
+                               args = (Value, a,))
+    client_process = Process(target = echo_client.echo_client,
+                               args = (message,))
     server_process.start()
     client_process.start()
     client_process.join()
--- echo_server.py
+++ echo_server.py
@@ -1,5 +1,7 @@
 import socket
 
+results = []
+
 def server_start():
     server_socket = socket.socket(
         socket.AF_INET,
@@ -9,10 +11,11 @@ def server_start():
     server_socket.bind((socket.gethostbyname(socket.gethostname()), 50000))
     return server_socket
 
-def server_run(server_socket):
+def server_run(num, server_socket):
+    num.value = 1.0
     server_socket.listen(1)
     conn, addr = server_socket.accept()
-
-    print "Client: " + conn.recv(32)
-
-    conn.sendall("Yes, i hear you.")
+    line = str(conn.recv(32))
+    print "Client: " + line
+    results.append(line)
+    conn.sendall(line)
