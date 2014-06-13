e9bd0b56e619526c670293c01ec8f78c4f09a85c
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-13 13:56:37 -0700

    modified echo client to work better with tests modified tests to work

--- echo_client.py
+++ echo_client.py
@@ -9,17 +9,22 @@ class client_class():
                     socket.SOCK_STREAM,
                     socket.IPPROTO_IP)
         self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.client_socket.connect((socket.gethostbyname(socket.gethostname()), 50000))
-        
+
 
     def client_run(self, message):
+        self.client_socket.connect((socket.gethostbyname(socket.gethostname()), 8000))
         message.encode('utf-8')
         self.client_socket.sendall(message)
         self.client_socket.shutdown(socket.SHUT_WR)
         data_receive = ""
         while 1:
             data = self.client_socket.recv(32)
+            data_receive += data
             if not data:
                 break
-            data_receive += data
-        print "Server: " + data_receive.decode('utf-8')
+        self.client_socket.close()
+        return data_receive.decode('utf-8')
+
+
+#client = client_class()
+#print client.client_run("GET /hello.html HTTP/1.1\r\n\r\n thIS IS THE body text it has to be a bit longer")
--- test_http_server.py
+++ test_http_server.py
@@ -1,29 +1,50 @@
 import echo_server
+import echo_client
+import time
+from multiprocessing import Process, Value
 
-server = Server()
-server.server_run()
+
+
+print "begin"
+server = echo_server.Server_class()
+print "server created"
+server_process = Process(target = server.server_run)
+server_process.start()
+#time.sleep(5)
+print "running server"
+
 
 def test_returnError():
+    print "started first test"
     error_message = echo_server.returnError("There's nothing here.")
-    assert error_message[:24] = "HTTP/1.1 400 BAD REQUEST"
+    assert error_message[:24] == "HTTP/1.1 400 BAD REQUEST"
 
 
 def test_bad_URI():
     """This error should be returned because there is no '/' before the hello.html file"""
-    client = client_class()
+    client = echo_client.client_class()
+ #   time.sleep(1)
     returnMessage = client.client_run("GET hello.html HTTP/1.1\r\n\r\n")
     assert returnMessage == "HTTP/1.1 <h1> 440 - COOL STORY BRO </h1>"
 
 
 def test_good_URI():
-    client = client_class()
+    client = echo_client.client_class()
+ #   time.sleep(1)
     returnMessage = client.client_run("GET /hello.html HTTP/1.1\r\n\r\n")
     assert returnMessage[len("HTTP/1.1 200 OK")] == "HTTP/1.1 200 OK"
 
 
 def test_bad_protocol():
-    client = client_class()
+    client = echo_client.client_class()
+ #   time.sleep(1)
     returnMessage = client.client_run("GET /hello.html HTT1.1\r\n\r\n")
     assert returnMessage == "HTTP/1.1 <h1> 430 - WRONG PROTOCOL </h1>"
+
+
+def test_injection_attack():
+    client = echo_client.client_class()
+    returnMessage = client.client_run("GET /../hello.html HTT1.1\r\n\r\n")
+    assert returnMessage == "HTTP/1.1 <h1> 510 - FILE NOT FOUND </h1>"
 
 
